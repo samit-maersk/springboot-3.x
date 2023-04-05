6afd5035cbe9264ac86fd4c98e70577c3800d8bd
samit-maersk <SamitKumar.Patel@maersk.com>
2023-04-05 16:36:53 +0200

    Initial commit

---
+++ src/main/java/net/samitkumar/springbootthree/SpringbootThreeApplication.java
@@ -0,0 +1,13 @@
+package net.samitkumar.springbootthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/samitkumar/springbootthree/SpringbootThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.springbootthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
